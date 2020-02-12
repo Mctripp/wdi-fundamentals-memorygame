8ba53055ff03a46786a9472312b0ce81b29730e3
Michael Tripp <michaeltripp@Michaels-MacBook-Pro.local>
2020-02-12 15:58:22 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,16 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardInPlay = [];
-var cardOne = cards[0];
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
 cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+console.log("User flipped " + cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo)
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} 
+	else {
+		alert("Sorry, try again.");
+	}
+}
