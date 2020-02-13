13e1fd5533c28f845b36427bac8a060b4a7a36d1
Michael Tripp <michaeltripp@Michaels-MacBook-Pro.local>
2020-02-13 16:05:51 -0500

    created game logic, added border to selected card, functionality to nav bar

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -9,6 +9,11 @@ const cards = [
 		suit: "diamonds",
 		cardImage: "images/queen-of-diamonds.png"
 	},
+		{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
 	{
 		rank: "king",
 		suit: "hearts",
@@ -18,24 +23,90 @@ const cards = [
 		rank: "king",
 		suit: "diamonds",
 		cardImage: "images/king-of-diamonds.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+		{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
 	}
 ];
 let cardsInPlay = [];
 
+function resetCard(cardImage1, cardImage2){
+	for(let i = 0; i < cards.length; i++){
+		let cardElement = document.querySelectorAll('.playingCard')[i];
+		if((cardElement.getAttribute('src') === cardImage1)
+			|| (cardElement.getAttribute('src') === cardImage2)){
+			cardElement.setAttribute('src', 'images/back.png');
+		}
+	}
+}
+
 function checkForMatch(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+	if((cardsInPlay[0].rank === cardsInPlay[1].rank)
+		&&(cardsInPlay[0].suit === cardsInPlay[1].suit)){
+		document.getElementById('game-state').textContent = 'You found a match!';
+		cardsInPlay.pop();
+		cardsInPlay.pop();
 	} 
 	else {
-		alert("Sorry, try again.");
+		document.getElementById('game-state').textContent = 'Sorry, try again.';
+		let cardImage1 = cardsInPlay[0].cardImage;
+		let cardImage2 = cardsInPlay[1].cardImage;
+		cardsInPlay.pop();
+		cardsInPlay.pop();
+		setTimeout(resetCard,2000, cardImage1, cardImage2);
 	}	
 }
 
-function flipCard(cardId){
+function flipCard(cardElement){
+	let cardId = this.getAttribute('data-id');
+	if(this.getAttribute('src') !== 'images/back.png'){
+		document.getElementById('game-state').textContent = 'Please pick a face-down card.';
+		return;
+	}
 	console.log("User flipped " + cards[cardId].rank + ".");
 	cardsInPlay.push(cards[cardId]);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay[0] && cardsInPlay[1]){
+		checkForMatch();
+	} else {
+		document.getElementById('game-state').textContent = 'Choose your second card.';
+	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for(let i = 0; i < cards.length; i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('class', 'playingCard');
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+function resetBoard(){
+	for(let i = 0; i < cards.length; i++){
+		let cardElement = document.querySelectorAll('.playingCard')[i];
+		console.log("Card " + i + " reset.");
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.addEventListener('click', flipCard);
+		cardsInPlay = [];
+		document.getElementById('game-state').textContent = 'Go again! Pick your first card.';
+	}
+}
+
+createBoard();
+
+document.getElementById('reset-button').addEventListener('click', resetBoard);
